54260f43ae30cdd2bdaa6da2c611b0c4120bc9c1
vaajnur <ainur.valiev@mail.ru>
2018-08-02 10:31:34 +0300

    webdav

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?
 use Sabre\DAV\Client;
 
 include 'vendor/autoload.php';
@@ -9,12 +10,13 @@ $settings = array(
     // 'proxy' => 'locahost:8888',
 );
 
+$LOCAL_back_folder = 'backups';
+$YA_back_folder = 'phpjs_backup';
+
 $client = new Client($settings);
 
-//////////// DELETE OLD
-
 $response =  $client->propfind(
-	'backups', // dir
+	$YA_back_folder, // dir
 	array(
     '{DAV:}displayname',
     '{DAV:}creationdate',
@@ -24,8 +26,8 @@ $response =  $client->propfind(
 
 function compareByTimeStamp($time1, $time2)
 {
-    if (strtotime($time1) < strtotime($time2))
-        return 1;
+    if (strtotime($time1) < strtotime($time2)) 
+    	return 1; 
     else if (strtotime($time1) > strtotime($time2)) 
         return -1;
     else
@@ -35,8 +37,25 @@ function compareByTimeStamp($time1, $time2)
 array_shift($response);
 $dates = array_column($response, '{DAV:}displayname');
 
-sort($dates, "compareByTimeStamp");
+usort($dates, "compareByTimeStamp");
+$dates = array_reverse($dates);
 
+//////////// DELETE OLD
  // $method, $url = '', $body = null, array $headers = []
-$response = $client->request('DELETE', 'dnkayuqs_backups/'.$dates[0] , null, array());
-print_r($response['statusCode']) . "\n";
+$response = $client->request('DELETE', "$YA_back_folder/".$dates[0] , null, array());
+
+$backup_date_arr = scandir( dirname(__DIR__) . "/$LOCAL_back_folder");
+// избавляемся от родит. директорий
+$backup_date = $backup_date_arr[2];
+$backup_files = scandir(dirname(__DIR__) . "/$LOCAL_back_folder/" . $backup_date);
+unset($backup_files[0]);
+unset($backup_files[1]);
+
+$response = $client->request('MKCOL', "$YA_back_folder/". $backup_date ,  null);
+foreach ($backup_files as $key => $backup_file) {
+	$file = fopen(dirname(__DIR__) ."/$LOCAL_back_folder/" . $backup_date . "/$backup_file", 'r');
+	$url = "$YA_back_folder/{$backup_date}/$backup_file";
+        // заменяем пробелы дабы не было 400 ошибки (Bad Message 400 reason: Unknown Version)
+	$url = str_replace ( ' ', '%20', $url);
+	$response = $client->request('PUT',  $url,  $file );
+}
